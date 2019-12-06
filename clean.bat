ac0a5dda0dd93f3d852b6c2d148c446e78ae4c3c
XsTr0k3 <57575812+XsTr0k3@users.noreply.github.com>
2019-12-06 04:58:51 -0800

    Update clean.bat

--- clean.bat
+++ clean.bat
@@ -1,4 +1,4 @@
-﻿@echo off
+@echo off
 del /s /f /q c:\windows\temp\*.*
 rd /s /q c:\windows\temp
 md c:\windows\temp
@@ -11,10 +11,18 @@ deltree /y c:\windows\temp
 deltree /y c:\windows\tmp
 deltree /y c:\windows\ff*.tmp
 deltree /y c:\windows\history
+deltree /y c:\windows\prefetch
 deltree /y c:\windows\cookies
 deltree /y c:\windows\recent
 deltree /y c:\windows\spool\printers
 del c:\WIN386.SWP
 rd /s /q "c:\windows\prefecth"
 md "c:\windows\prefecth"
-cls 
+
+ del /s /f /q c:\windows\temp\*.*
+ rd /s /q c:\windows\temp
+ md c:\windows\temp
+ del /s /f /q C:\WINDOWS\Prefetch
+ del /s /f /q %temp%\*.*
+ rd /s /q %temp%
+ md %temp%
